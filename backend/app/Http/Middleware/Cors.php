b71a921084674655934bf8ab509a9847e7a8aa6c
urielrivero <urielrivps@gmail.com>
2024-10-24 01:52:47 -0300

    refactor: cors

--- backend/app/Http/Middleware/Cors.php
+++ backend/app/Http/Middleware/Cors.php
@@ -4,14 +4,21 @@ namespace App\Http\Middleware;
 
 use Closure;
 
-class Cors {
-    
-    public function handle($request, Closure $next) {
-        
+class Cors
+{
+    public function handle($request, Closure $next)
+    {
         $response = $next($request);
-        $response->headers->set('Access-Control-Allow-Origin', '*');
-        $response->headers->set('Access-Control-Allow-Methods', 'POST, GET');
-        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With, Application', 'ip');
-        return $response; 
+        $response->headers->set("Access-Control-Allow-Origin", "*");
+        $response->headers->set(
+            "Access-Control-Allow-Methods",
+            "POST, GET, PATCH, DELETE, OPTIONS"
+        );
+        $response->headers->set(
+            "Access-Control-Allow-Headers",
+            "Content-Type, Accept, Authorization, X-Requested-With, Application",
+            "ip"
+        );
+        return $response;
     }
 }
